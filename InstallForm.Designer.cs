3123e453f97788b7b4340fc9af2b2df7c86bf1b3
jesse <delsen_web@163.com>
2015-03-28 16:20:16 +0800

    init RosinInstall

---
+++ InstallForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RosinInstall
+{
+    partial class InstallForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstallForm));
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelIntro = new System.Windows.Forms.Label();
+            this.labelGuide = new System.Windows.Forms.Label();
+            this.buttonPre = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelInstall = new System.Windows.Forms.Label();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("微软雅黑", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitle.Location = new System.Drawing.Point(161, 18);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(170, 19);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "欢迎使用Rosin安装向导";
+            // 
+            // labelIntro
+            // 
+            this.labelIntro.AutoSize = true;
+            this.labelIntro.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelIntro.Location = new System.Drawing.Point(165, 65);
+            this.labelIntro.Name = "labelIntro";
+            this.labelIntro.Size = new System.Drawing.Size(356, 119);
+            this.labelIntro.TabIndex = 1;
+            this.labelIntro.Text = "感谢您使用Rosin\r\n\r\nRosin是一款Fiddler日志插件，我们将不断更新以保证它的稳定性\r\n并始终保持简洁，感谢您的支持\r\n\r\n使用中遇到任何问题请联系" +
+    "我们：\r\njessedeng、yunshengli、azraellong";
+            // 
+            // labelGuide
+            // 
+            this.labelGuide.AutoSize = true;
+            this.labelGuide.Location = new System.Drawing.Point(168, 208);
+            this.labelGuide.Name = "labelGuide";
+            this.labelGuide.Size = new System.Drawing.Size(149, 12);
+            this.labelGuide.TabIndex = 2;
+            this.labelGuide.Text = "请点击“下一步”继续安装";
+            // 
+            // buttonPre
+            // 
+            this.buttonPre.Location = new System.Drawing.Point(393, 250);
+            this.buttonPre.Name = "buttonPre";
+            this.buttonPre.Size = new System.Drawing.Size(75, 23);
+            this.buttonPre.TabIndex = 3;
+            this.buttonPre.Text = "上一步";
+            this.buttonPre.UseVisualStyleBackColor = true;
+            this.buttonPre.Click += new System.EventHandler(this.buttonPre_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(485, 250);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "下一步";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(578, 250);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "取消";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelInstall
+            // 
+            this.labelInstall.AutoSize = true;
+            this.labelInstall.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelInstall.Location = new System.Drawing.Point(272, 121);
+            this.labelInstall.Name = "labelInstall";
+            this.labelInstall.Size = new System.Drawing.Size(101, 17);
+            this.labelInstall.TabIndex = 6;
+            this.labelInstall.Text = "安装中，请稍后...";
+            this.labelInstall.Visible = false;
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Location = new System.Drawing.Point(485, 251);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone.TabIndex = 7;
+            this.buttonDone.Text = "完成";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            this.buttonDone.Visible = false;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // InstallForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 291);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.labelInstall);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPre);
+            this.Controls.Add(this.labelGuide);
+            this.Controls.Add(this.labelIntro);
+            this.Controls.Add(this.labelTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InstallForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rosin安装向导";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelIntro;
+        private System.Windows.Forms.Label labelGuide;
+        private System.Windows.Forms.Button buttonPre;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelInstall;
+        private System.Windows.Forms.Button buttonDone;
+    }
+}
+
---
+++ InstallForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace RosinInstall
+{
+    public partial class InstallForm : Form
+    {
+        public InstallForm()
+        {
+            InitializeComponent();
+
+            this.buttonPre.Enabled = false;
+        }
+
+        // 拷贝Rosin.dll到script目录
+        // 拷贝Newtonsoft.Json.dll到script目录
+        // 拷贝Rosin目录到script目录
+        // 用于项目首次阶段，或者初始化
+        private bool CopyAllFileToPath()
+        {
+            bool isInstallSuccess = false;
+
+            string fiddlerInstallDir = this.GetFiddlerDir();
+            string currentDir = System.IO.Path.GetFullPath(".\\");
+
+            // 如果通过注册表没有获取到fiddler的安装路径，让用户去选择
+            if (fiddlerInstallDir == "")
+            {
+                fiddlerInstallDir = this.SelectFiddlerDir();
+            }
+
+            if (fiddlerInstallDir != "")
+            {
+                currentDir += @"\Scripts";
+                string scriptDir = fiddlerInstallDir + @"\Scripts";
+
+                string[] fileList = { "Rosin.dll", "Newtonsoft.Json.v6.0.dll" };
+                string[] dirList = { "Rosin" };
+
+                string[] fileNoOverwriteList = { "InjectionList.xml", "InjectionRule.xml" };
+
+                foreach (string fileName in fileList)
+                {
+                    string sourceFile = System.IO.Path.Combine(currentDir, fileName);
+                    string destFile = System.IO.Path.Combine(scriptDir, fileName);
+                    if (System.IO.File.Exists(sourceFile))
+                    {
+                        try
+                        {
+                            System.IO.File.Copy(sourceFile, destFile, true);
+                        }
+                        catch
+                        {
+                            throw new Exception("安装失败，请关闭Fiddler后再安装!");
+                        }
+                    }
+                }
+
+                foreach (string dirName in dirList)
+                {
+                    string sourceDir = System.IO.Path.Combine(currentDir, dirName);
+                    string destDir = System.IO.Path.Combine(scriptDir, dirName);
+                    this.CopyDirectory(sourceDir, destDir, fileNoOverwriteList);
+                }
+
+                isInstallSuccess = true;
+            }
+
+            return isInstallSuccess;
+        }
+
+        private void CopyDirectory(string srcDir, string tgtDir, string[] fileNoOverwriteList)
+        {
+            DirectoryInfo source = new DirectoryInfo(srcDir);
+            DirectoryInfo target = new DirectoryInfo(tgtDir);
+
+            if (target.FullName.StartsWith(source.FullName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new Exception("父目录不能拷贝到子目录！");
+            }
+
+            if (!source.Exists)
+            {
+                return;
+            }
+
+            if (!target.Exists)
+            {
+                target.Create();
+            }
+
+            FileInfo[] files = source.GetFiles();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                bool isOverWrite = true;
+
+                foreach (string name in fileNoOverwriteList)
+                {
+                    if (name == files[i].Name)
+                    {
+                        isOverWrite = false;
+                        break;
+                    }
+                }
+
+                if (isOverWrite || !File.Exists(target.FullName + @"\" + files[i].Name))
+                {
+                    File.Copy(files[i].FullName, target.FullName + @"\" + files[i].Name, true);
+                }
+            }
+
+            DirectoryInfo[] dirs = source.GetDirectories();
+
+            for (int j = 0; j < dirs.Length; j++)
+            {
+                this.CopyDirectory(dirs[j].FullName, target.FullName + @"\" + dirs[j].Name, fileNoOverwriteList);
+            }
+        }
+
+        private string GetFiddlerDir()
+        {
+            string fiddlerInstallPath = "";
+            string fiddlerInstallDir = "";
+
+            try
+            {
+                string softName = "Fiddler";
+                string strKeyName = String.Empty;
+                string softPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\";
+
+                RegistryKey regKey = Registry.LocalMachine;
+                RegistryKey regSubKey = regKey.OpenSubKey(softPath + softName + ".exe", false);
+
+                object objResult = regSubKey.GetValue(strKeyName);
+
+                RegistryValueKind regValueKind = regSubKey.GetValueKind(strKeyName);
+
+                if (regValueKind == Microsoft.Win32.RegistryValueKind.String)
+                {
+                    fiddlerInstallPath = objResult.ToString();
+                }
+
+            }
+            catch
+            {
+                fiddlerInstallPath = "";
+            }
+
+            if (fiddlerInstallPath != "")
+            {
+                fiddlerInstallPath = fiddlerInstallPath.Replace("\"", "");
+                fiddlerInstallDir = Path.GetDirectoryName(fiddlerInstallPath);
+            }
+
+            return fiddlerInstallDir;
+        }
+
+        private string SelectFiddlerDir()
+        {
+            string fiddlerInstallDir = "";
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "请选择Fiddler的安装路径";
+            folderBrowserDialog.ShowDialog();
+
+            fiddlerInstallDir = folderBrowserDialog.SelectedPath;
+
+            return fiddlerInstallDir;
+        }
+
+        private void buttonPre_Click(object sender, EventArgs e)
+        {
+            this.buttonPre.Enabled = false;
+            this.buttonNext.Enabled = true;
+            this.buttonCancel.Enabled = true;
+
+            this.labelIntro.Visible = true;
+            this.labelGuide.Visible = true;
+            this.labelInstall.Visible = false;
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            this.labelInstall.Text = "安装中，请稍后...";
+
+            this.buttonPre.Enabled = false;
+            this.buttonNext.Enabled = false;
+            this.buttonCancel.Enabled = false;
+
+            this.buttonPre.Visible = false;
+            this.buttonNext.Visible = false;
+            this.buttonCancel.Visible = false;
+
+            this.labelIntro.Visible = false;
+            this.labelGuide.Visible = false;
+            this.labelInstall.Visible = true;
+
+            this.buttonDone.Enabled = false;
+            this.buttonDone.Visible = true;
+
+            bool result = this.CopyAllFileToPath();
+
+            if (result)
+            {
+                this.labelInstall.Text = "安装完成，感谢使用!";
+                this.buttonDone.Enabled = true;
+            }
+            else
+            {
+                this.labelInstall.Text = "安装失败，请重新安装!";
+                this.buttonDone.Enabled = true;
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
